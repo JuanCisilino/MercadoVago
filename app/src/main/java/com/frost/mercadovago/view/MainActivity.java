2b7486d6fe65d782bc7ce0a2fa6d6ee282ff216f
Juan C <juan.cisilino@gmail.com>
2020-01-05 21:20:48 -0300

    implemento librerias
    material design
    glide
    retrofit2
    androidx

--- app/src/main/java/com/frost/mercadovago/view/MainActivity.java
+++ app/src/main/java/com/frost/mercadovago/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.frost.mercadovago;
+package com.frost.mercadovago.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.frost.mercadovago.R;
 
 public class MainActivity extends AppCompatActivity {
 
